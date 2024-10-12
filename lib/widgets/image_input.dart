25588101b9e5f60f4eb0d5504fddcb2239ec0ba7
Owoeye Samuel <owoeye1321@gmail.com>
2024-10-12 14:44:00 +0100

    ☕️

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -14,16 +14,17 @@ class ImageInput extends StatefulWidget{
 class _ImageInput extends State<ImageInput>{
   _ImageInput();
 
+void _takePicture(){}
 
 @override
 Widget build(BuildContext context){
   return Container(
     height: 250,
-    decoration: BoxDecoration(border: Border.all(color: Theme.of(context).colorScheme.onSurface),),
+    decoration: BoxDecoration(border: Border.all(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.2))),
     margin: const EdgeInsets.symmetric(vertical: 15),
     alignment: Alignment.center,
     width: double.infinity,
-    child: TextButton.icon(onPressed: (){}, icon:const  Icon(Icons.camera), label: const Text("Take Picture")),
+    child: TextButton.icon(onPressed: _takePicture, icon:const  Icon(Icons.camera), label: const Text("Take Picture")),
   );
 }
 }

fcb4943c4fde996763d22e4a048d087d2345698e
Owoeye Samuel <owoeye1321@gmail.com>
2024-10-21 22:42:18 +0100

    nice piece! here

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,40 +2,54 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-class ImageInput extends StatefulWidget{
+class ImageInput extends StatefulWidget {
   ImageInput({super.key});
-  
-  @override
-  State<ImageInput> createState()=> _ImageInput();
 
+  @override
+  State<ImageInput> createState() => _ImageInput();
 }
 
-
-class _ImageInput extends State<ImageInput>{
+class _ImageInput extends State<ImageInput> {
   File? _seletedImage;
 
+  void _takePicture() async {
+    //this function initiate the camera
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
 
-void _takePicture() async {
-  //this function initiate the camera
-  final imagePicker = ImagePicker();
-  final pickedImage = await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
-
-  if(pickedImage == null){
-    return;
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _seletedImage = File(pickedImage.path);
+    });
   }
-  _seletedImage = File(pickedImage.path);
 
-}
+  @override
+  Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      icon: const Icon(Icons.camera),
+      label: const Text("Take Picture"),
+    );
 
-@override
-Widget build(BuildContext context){
-  return Container(
-    height: 250,
-    decoration: BoxDecoration(border: Border.all(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.2))),
-    margin: const EdgeInsets.symmetric(vertical: 15),
-    alignment: Alignment.center,
-    width: double.infinity,
-    child: TextButton.icon(onPressed: _takePicture, icon:const  Icon(Icons.camera), label: const Text("Take Picture")),
-  );
-}
+    if (_seletedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(_seletedImage!,
+            fit: BoxFit.cover, width: double.infinity, height: double.infinity),
+      );
+    }
+    return Container(
+      height: 250,
+      decoration: BoxDecoration(
+          border: Border.all(
+              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.2))),
+      margin: const EdgeInsets.symmetric(vertical: 15),
+      alignment: Alignment.center,
+      width: double.infinity,
+      child: content,
+    );
+  }
 }
